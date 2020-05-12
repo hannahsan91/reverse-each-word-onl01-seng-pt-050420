11649bd4b5a77f493737e00d5cfd5cbce26b3532
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 02:03:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
 sentence1 = "Hello there, and how are you?"
 def reverse_each_word(sentence1)
   reverse_each_word.each do |sentence1|
-    puts "olleH ,ereht dna woh era ?uoy"
   end
+end
+
+sentence2 = "Hi again, just making sure it's reversed!"
+def reverse_each_word(sentence2)
+end
 end
