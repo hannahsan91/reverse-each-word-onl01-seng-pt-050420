2da772c6f8f6ed74c6a8d7cb88ac334701a9c2d2
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 02:18:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,24 +1,3 @@
-sentence1 = "Hello there, and how are you?"
-def reverse_each_word(sentence1)
-  reverse_each_word.each do |sentence1|
-    puts "olleH ,ereht dna woh era ?uoy"
-  end
-end
-
-sentence2 = "Hi again, just making sure it's reversed!"
-def reverse_each_word(sentence2)
-  reverse_each_word.each do |sentence2|
-    puts "iH ,niaga tsuj gnikam erus s'ti !desrever"
-  end
-end
-
-def reverse_each_word(sentence1, sentence2)
-  reverse_each_word.collect do |sentence1, sentence2|
-    puts "Verifying that collect is being called"
-  end
-end
-
 def reverse_each_word(sentence)
   words_array = sentence1, sentence2
   new_array = []
-  
