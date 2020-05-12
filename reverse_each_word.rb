d95a1e179dd06238a4f854d670d22ce61bc85fb8
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 02:29:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def reverse_each_word(sentence)
   binding.pry
-  sentence.split
+  words_array = sentence.split
   new_array = []
   words_array = words_array.collect {|word| word.reverse}
 end
