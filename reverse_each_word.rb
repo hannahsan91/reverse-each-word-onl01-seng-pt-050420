8eacf58bd82d8dd6220842c20c637cc91712d872
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 02:29:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ def reverse_each_word(sentence)
   binding.pry
   words_array = sentence.split
   new_array = []
-  words_array = words_array.collect {|word| word.reverse}
+  words_array.collect {|word| word.reverse}
 end
